b088b37800c78711dc4184f6a0113e8483e35ea9
BrunoImai <47978966+BrunoImai@users.noreply.github.com>
2021-11-05 17:15:26 -0300

    Carousel created
    - first Carousel implementation

--- MotionMusic/views/Home/HomeViewController.swift
+++ MotionMusic/views/Home/HomeViewController.swift
@@ -6,15 +6,71 @@
 //
 
 import UIKit
+import iCarousel
 
 var DEBUG_MODE = false
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
 
     //MARK: OUTLETS
     @IBOutlet weak var InterfaceView: UIView!
 
     @IBOutlet weak var SeeAreasButton: UIButton!
+    
+    @IBOutlet weak var Carousel: UIView!
+    
+    //MARK: CAROUSEL CONFIGURATION
+    
+    let carousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .linear
+        return view
+    }()
+    
+    func setupCarousel(){
+        carousel.frame = Carousel.frame
+        carousel.dataSource = self
+        carousel.delegate = self
+        carousel.stopAtItemBoundary = true
+
+        print("carousel criado")
+        carousel.scrollToItem(at: (0), animated: true)
+        
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return mockMusics.count
+    }
+    
+    func carouselCurrentItemIndexDidChange(_ carousel: iCarousel) {
+        carousel.reloadData()
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        switch (option) {
+        case .spacing: return 1.5 // 1.5 points spacing
+        
+//        case .visibleItems: return 11
+
+            default: return value
+        }
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        
+        let view = setupCarouselItemView(item: mockMusics[index])
+        return view
+    }
+    
+    func setupCarouselItemView(item : MusicModel) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        view.backgroundColor = item.color
+        view.layer.cornerRadius = view.frame.width / 2
+        view.clipsToBounds = true
+        
+        return view
+    }
+    
     
     //MARK: VARIABLES
     private var seeAreas: Bool = true {
@@ -30,6 +86,9 @@ class HomeViewController: UIViewController {
     //MARK: LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Carousel.addSubview(carousel)
+        setupCarousel()
         
         self.view.bringSubviewToFront(self.InterfaceView)
         // Do any additional setup after loading the view.
