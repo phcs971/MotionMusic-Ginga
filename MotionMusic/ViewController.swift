52cab13f638a0d6e09a1264052ca99fbe51199b1
phcs971 <phcs.971@gmail.com>
2021-11-05 14:27:49 -0300

    Initial Commit

---
+++ MotionMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionMusic
+//
+//  Created by Pedro Henrique Cordeiro Soares on 05/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
