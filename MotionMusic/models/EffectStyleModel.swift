edf932277c47d35793fdf9d191443e7784a584be
BrunoImai <47978966+BrunoImai@users.noreply.github.com>
2021-11-22 16:58:12 -0300

    effects views updated

--- MotionMusic/models/EffectStyleModel.swift
+++ MotionMusic/models/EffectStyleModel.swift
@@ -14,12 +14,13 @@ struct EffectStyleModel: Equatable, Identifiable {
     
     var name: String
     
-    var color: UIColor
+    var colors: [UIColor]
     
 }
 
 let mockEffects = [
-    EffectStyleModel(name: "Basico", color: .gray),
-    EffectStyleModel(name: "Sunset", color: .red),
-    EffectStyleModel(name: "Trevoso", color: .purple),
+    EffectStyleModel(name: "Basico", colors: [.gray, .gray, .gray]),
+    EffectStyleModel(name: "Sunset", colors: [#colorLiteral(red: 1, green: 0.9374610782, blue: 0.3620254695, alpha: 1), #colorLiteral(red: 0.9888160825, green: 0.6351911426, blue: 0.3159540892, alpha: 1), #colorLiteral(red: 0.9905835986, green: 0.3816677928, blue: 0.5077443719, alpha: 1)]),
+    EffectStyleModel(name: "Trevoso", colors: [#colorLiteral(red: 0.05610793829, green: 0.02776246145, blue: 0.1231660023, alpha: 1), #colorLiteral(red: 0.2785869837, green: 0.4369585812, blue: 0.9991567731, alpha: 1), #colorLiteral(red: 0.9522402883, green: 0.4175275266, blue: 1, alpha: 1)])
 ]
+
