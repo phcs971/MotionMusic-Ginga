cc3511b8e5e2a9a7832d0a0887bf2f3172e813f1
phcs971 <phcs.971@gmail.com>
2021-11-25 09:17:50 -0300

    Remove Idle Timer

--- MotionMusic/AppDelegate.swift
+++ MotionMusic/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-        
+        UIApplication.shared.isIdleTimerDisabled = true
         SettingsService.instance.start()
         
         return true
